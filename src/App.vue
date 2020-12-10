cea8751aa7639908c83e7fb60454cf2bfb7980a4
Brandon Parkinson <brandonjamesparkinson@gmail.com>
2020-12-10 16:10:15 +0000

    + added style & logic for calculating total income

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-  <h1> Hello, World</h1>
+  <Header :totalIncome="state.totalIncome"/>
 </template>
 
 <script>
+import { reactive, computed } from 'vue';
+import Header from './components/Header'
 
 export default {
+  setup() {
+    const state = reactive ({
+      income: [],
+      totalIncome: computed(() => {
+        let temp = 0;
 
+        if (state.income.length > 0) {
+          for (let i = 0; i < state.income.length; i++){
+            temp += state.income[i];
+          }
+        }
+
+        return temp;
+      })
+    });
+
+    return {
+      Header,
+      state
+    }
+  }
 }
 
 </script>
